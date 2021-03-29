744e7c6c9d883ce06718ce9f44e6c57ec7a233e6
Jen Weber <j@jenweber.me>
2021-03-29 14:20:59 -0400

    demo of didLoad

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,6 @@ export default class IndexRoute extends Route {
 
     model() {
         console.log('model hook')
-        store.createRecord('article', { id: 1});
        return this.store.findRecord('article', 1)  // => GET /blog-posts/1
         .then(function(blogPost) {
             // Do something with `blogPost`
